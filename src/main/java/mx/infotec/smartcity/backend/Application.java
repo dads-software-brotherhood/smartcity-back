81add031cd72ceebc59d86851337aea4fdd9afde
Erik Valdivieso <erik.valdivieso@gmail.com>
2017-03-08 23:08:02 -0600

    Add profile for production and development

--- src/main/java/mx/infotec/smartcity/backend/Application.java
+++ src/main/java/mx/infotec/smartcity/backend/Application.java
@@ -2,6 +2,8 @@ package mx.infotec.smartcity.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author infotec
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -23,7 +25,12 @@ public class Application {
             }
         };
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
