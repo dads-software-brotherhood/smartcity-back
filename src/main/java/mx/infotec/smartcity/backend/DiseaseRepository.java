be38e3e8d9b214deb19d839af69158c0ae9984e1
INFOTEC\adrian.molina <infotec2016>
2017-03-16 18:19:15 -0600

    Diasese Service

---
+++ src/main/java/mx/infotec/smartcity/backend/DiseaseRepository.java
@@ -0,0 +1,14 @@
+package mx.infotec.smartcity.backend;
+
+import mx.infotec.smartcity.backend.model.Disease;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author adrian
+ */
+public interface DiseaseRepository extends MongoRepository<Disease, String> {
+
+    Disease findByName(String name);
+}
