9639a9b06f7012fd62fc26ab1946a4980a23c999
benjivds <benji_vds@yahoo.com>
2017-03-21 11:02:27 -0600

    First draw of Public Transport proof of concept with paging

---
+++ src/main/java/mx/infotec/smartcity/backend/model/transport/ProfilePublicTransport.java
@@ -0,0 +1,62 @@
+
+package mx.infotec.smartcity.backend.model.transport;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"id", "idProfile", "idPublicTransport"})
+public class ProfilePublicTransport {
+
+  public ProfilePublicTransport() {
+
+  }
+
+  public ProfilePublicTransport(String idProfile, String idPublicTransport) {
+    this.idProfile = idProfile;
+    this.idPublicTransport = idPublicTransport;
+  }
+
+  @JsonProperty("id")
+  private String id;
+  @JsonProperty("idProfile")
+  private String idProfile;
+
+  @Indexed(unique = true)
+  @JsonProperty("idPublicTransport")
+  private String idPublicTransport;
+
+  @JsonProperty("id")
+  public String getId() {
+    return id;
+  }
+
+  @JsonProperty("id")
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  @JsonProperty("idProfile")
+  public String getIdProfile() {
+    return idProfile;
+  }
+
+  @JsonProperty("idProfile")
+  public void setIdProfile(String idProfile) {
+    this.idProfile = idProfile;
+  }
+
+  @JsonProperty("idPublicTransport")
+  public String getIdPublicTransport() {
+    return idPublicTransport;
+  }
+
+  @JsonProperty("idPublicTransport")
+  public void setIdPublicTransport(String idPublicTransport) {
+    this.idPublicTransport = idPublicTransport;
+  }
+
+}
---
+++ src/main/java/mx/infotec/smartcity/backend/model/transport/Stops.java
@@ -0,0 +1,41 @@
+
+package mx.infotec.smartcity.backend.model.transport;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "type",
+    "coordinates"
+})
+public class Stops {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("coordinates")
+    private List<List<Double>> coordinates = null;
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("coordinates")
+    public List<List<Double>> getCoordinates() {
+        return coordinates;
+    }
+
+    @JsonProperty("coordinates")
+    public void setCoordinates(List<List<Double>> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+}
