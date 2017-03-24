88cd6c7d2a4f18e210e27aef4e4919fbdd0cec03
benjivds <benji_vds@yahoo.com>
2017-03-24 07:52:38 -0600

    changed   registration.addUrlPatterns("/customers/*"); to   registration.addUrlPatterns("/customers/**");

--- src/main/java/mx/infotec/smartcity/backend/Application.java
+++ src/main/java/mx/infotec/smartcity/backend/Application.java
@@ -1,7 +1,7 @@
 package mx.infotec.smartcity.backend;
 
 import javax.servlet.Filter;
-import mx.infotec.smartcity.backend.filter.LoggedUserFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import mx.infotec.smartcity.backend.filter.LoggedUserFilter;
+
 /**
  *
  * @author infotec
@@ -19,40 +21,41 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600);
-            }
-        };
-    }
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").maxAge(3600);
+      }
+    };
+  }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
 
-    @Bean
-    public FilterRegistrationBean loggedUserFilterRegistration() {
+  @Bean
+  public FilterRegistrationBean loggedUserFilterRegistration() {
 
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(loggedUserFilter());
-        registration.addUrlPatterns("/customers/*");
-        //registration.addInitParameter("paramName", "paramValue");
-        registration.setName("loggedUserFilter");
-        registration.setOrder(1);
-        
-        return registration;
-    }
+    FilterRegistrationBean registration = new FilterRegistrationBean();
+    registration.setFilter(loggedUserFilter());
+    registration.addUrlPatterns("/customers/**");
+    // registration.addInitParameter("paramName", "paramValue");
+    registration.setName("loggedUserFilter");
 
-    @Bean(name = "loggedUserFilter")
-    public Filter loggedUserFilter() {
-        return new LoggedUserFilter();
-    }
+    registration.setOrder(1);
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    return registration;
+  }
+
+  @Bean(name = "loggedUserFilter")
+  public Filter loggedUserFilter() {
+    return new LoggedUserFilter();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
