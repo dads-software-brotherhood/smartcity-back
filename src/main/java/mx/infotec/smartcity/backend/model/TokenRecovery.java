3036176b319980b5c562ebba05f251d80d312b05
Rodrigo Nievez <ivan.nievez@infotec.mx>
2017-03-23 18:35:11 -0600

    Integración de correos y recuperación de contraseña

---
+++ src/main/java/mx/infotec/smartcity/backend/model/TokenRecovery.java
@@ -0,0 +1,69 @@
+package mx.infotec.smartcity.backend.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import org.springframework.data.annotation.Id;
+
+public class TokenRecovery implements Serializable{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+  
+  @Id
+  private String id;
+  
+  private String idUser;
+  
+  private String email;
+  
+  private Date registerDate;
+  
+  public TokenRecovery() {
+    
+  }
+
+  public TokenRecovery(String idUser, String email, Date registerDate) {
+    super();
+    this.idUser = idUser;
+    this.email = email;
+    this.registerDate = registerDate;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getIdUser() {
+    return idUser;
+  }
+
+  public void setIdUser(String idUser) {
+    this.idUser = idUser;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getRegisterDate() {
+    return registerDate;
+  }
+
+  public void setRegisterDate(Date registerDate) {
+    this.registerDate = registerDate;
+  }
+  
+  
+
+}
