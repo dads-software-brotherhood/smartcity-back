072a511b0e61204b618c73b1bb69bfd4b7bc91e8
Erik Valdivieso <erik.valdivieso@infotec.mx>
2017-03-27 18:39:09 -0600

    Fix error problem

--- src/main/scripts/sample-data/import.sh
+++ src/main/scripts/sample-data/import.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 HOST=localhost
-PORT=37017
+PORT=27017
 DATABASE=smartcity
 
-if [ -f 'country.json' ]; then
-    mongoimport --host $HOST --port $PORT --collection $DATABASE --db country --drop --file country.json
-fi
+function loadFile() {
+    if [ -f $2 ]; then
+        echo "Import $2 file on $1 collection"
+        mongoimport --host $HOST --port $PORT --db $DATABASE --collection $1 --drop --file $2
+    else
+        echo "File $2 not found"
+    fi
+}
 
-if [ -f 'regions.json' ]; then
-    mongoimport --host $HOST --port $PORT --collection $DATABASE --db regions --drop --file regions.json
-fi
-
-if [ -f 'locality.json' ]; then
-    mongoimport --host $HOST --port $PORT --collection $DATABASE --db locality --drop --file locality.json
-fi
-
+loadFile 'country' 'country_file.json'
+loadFile 'region' 'region_file.json'
+loadFile 'locality' 'locality_file.json'
